949f0a603c92ae14ffe9e61e20c146b18481f25b
Armoury-07 <70344963+Armoury-07@users.noreply.github.com>
2020-09-09 14:59:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    volume_sphere = 4 / 3 * Pi * radius ** 3
+    basic.showNumber(volume_sphere)
+})
+let volume_sphere = 0
+let Pi = 0
+let radius = 0
+radius = 10
+Pi = 3.1416
